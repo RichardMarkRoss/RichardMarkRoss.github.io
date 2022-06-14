92e895e372ba50e39e49aa7f0f25f053ba6ba38f
RichardMarkRoss <cptrichkidprice@gmail.com>
2022-06-14 20:27:26 +0200

    old changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Router } from '@angular/router';
 // import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from './app.component';
