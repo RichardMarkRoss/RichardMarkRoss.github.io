dd6a6e51a1bad707567ed953b986196391ee69c1
Richard <cptrichkidprice@gmail.com>
2025-10-20 13:09:49 +0200

    👌 IMPROVE: upgrade to angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from  '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from  '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgChartsModule } from 'ng2-charts';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    routingComponents
-   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    NgChartsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        routingComponents
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        NgChartsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
