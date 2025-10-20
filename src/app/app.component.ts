de178bdbfb671c22db1baffb083b506b35e18330
Richard <cptrichkidprice@gmail.com>
2025-10-20 14:17:20 +0200

    👌 IMPROVE: Build version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { VERSION } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: false
 })
 export class AppComponent {
   title = 'profile';
+  angularVersion = VERSION.full;
+  appVersion = '1.3.2';
 }
-  
